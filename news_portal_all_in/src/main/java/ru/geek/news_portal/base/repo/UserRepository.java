238cbbe8b2678561d8482942eab62dd147824d86
DiNikOs <demon22odn08@rambler.ru>
2020-03-19 13:30:08 +0300

    week3. +security web filter

--- news_portal_all_in/src/main/java/ru/geek/news_portal/base/repo/UserRepository.java
+++ news_portal_all_in/src/main/java/ru/geek/news_portal/base/repo/UserRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.geek.news_portal.base.entities.User;
 
+import java.nio.channels.FileChannel;
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
   User findOneByUsername(String username);
 
   boolean existsByUsername(String username);
+  Optional<User> findUserByUsername(String username);
 }
