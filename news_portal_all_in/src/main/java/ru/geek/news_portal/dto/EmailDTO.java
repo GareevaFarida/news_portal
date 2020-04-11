b217ff5a680af0358a053fd656ec7e239ecf25b6
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-04-11 21:07:06 +0300

    Добавлена работа с пользователями (/users). Пока без security/ Не все пока работает. Надо допилить

---
+++ news_portal_all_in/src/main/java/ru/geek/news_portal/dto/EmailDTO.java
@@ -0,0 +1,12 @@
+package ru.geek.news_portal.dto;
+
+/**
+ * GeekBrains Java, news_portal.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 10.04.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class EmailDTO {
+}
